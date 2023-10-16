9572afbcb3d0b19e9f9a34ee67bed439cc748ead
Jackson Vanover <jacksonvanover@gmail.com>
2023-10-16 12:16:25 -0600

    build for derecho

---
+++ scripts/build_dwarp-p-cloudsc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TUNING_REPO_ROOT=$(realpath $(dirname $(realpath $0))/.. )
+cd $TUNING_REPO_ROOT
+
+./cloudsc-bundle build --clean --with-mpi --arch=./arch/ucar/derecho/intel/2023.0.0
